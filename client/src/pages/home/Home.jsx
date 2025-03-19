3eb770e79f4ecac4fd048a017670030e443818d3
navyasrisangem <85790258+navyasrisangem@users.noreply.github.com>
2025-03-19 12:41:56 +0530

    Update Home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,8 +2,8 @@ import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
-import PropertyList from "../../components/propertyList/propertyList";
-import FeaturedProperties from "../../components/featuredProperties/featuredProperties";
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import MailList from "../../components/mailList/mailList";
 import Footer from "../../components/footer/footer";
 
