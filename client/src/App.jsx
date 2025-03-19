0309d52a3bf8cb9041c182778d907521917d83fe
navyasrisangem <85790258+navyasrisangem@users.noreply.github.com>
2025-03-19 12:08:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Hotel from "./pages/hotel/Hotel";
 import Login from "./pages/login/Login";
-import Register from "./pages/register/register";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
