97628c6548e306aeebff8d5157a931c27cbf7244
navyasrisangem <85790258+navyasrisangem@users.noreply.github.com>
2025-03-20 18:33:57 +0530

    Update Reserve.jsx

--- api/client/src/components/reserve/Reserve.jsx
+++ api/client/src/components/reserve/Reserve.jsx
@@ -7,7 +7,7 @@ import { SearchContext } from "../../context/SearchContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://nestinn.onrender.com/api";
 
 const Reserve = ({ setOpen, hotelId }) => {
   const [selectedRooms, setSelectedRooms] = useState([]);
