81dfb18869914ad0812938cb10b07aad683a2831
navyasrisangem <85790258+navyasrisangem@users.noreply.github.com>
2025-03-20 18:35:32 +0530

    Update Login.jsx

--- api/client/src/pages/login/Login.jsx
+++ api/client/src/pages/login/Login.jsx
@@ -4,7 +4,7 @@ import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import "./login.css";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://nestinn.onrender.com/api";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
