c54c0f7dd36fbffc01b22fb4c2f0cb903e514a93
navyasrisangem <85790258+navyasrisangem@users.noreply.github.com>
2025-03-20 18:33:04 +0530

    Update FeaturedProperties.jsx

--- api/client/src/components/featuredProperties/FeaturedProperties.jsx
+++ api/client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://nestinn.onrender.com/api";
 
 const FeaturedProperties = () => {
   const navigate = useNavigate();
